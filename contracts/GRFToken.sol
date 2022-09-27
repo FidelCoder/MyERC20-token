c37bc2c688256499bc3473caabc7897669bdf7b2
Griffins Onyango Oduol <griffinesonyango@gmail.com>
2022-09-27 14:04:40 +0300

    adding contract changes GRF

--- contracts/GRFToken.sol
+++ contracts/GRFToken.sol
@@ -1,3 +1,9 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity >=0.4.25 <0.8.17;
-contract GRFToken{}
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract GRFToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("GRFToken", "GRF") {
+        _mint(msg.sender, initialSupply);
+    }
+}
